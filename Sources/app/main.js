c02909a37b3c9092dbbc7ad71f9ea567cc400186
Jean-Christophe Ricard <jeanchristophe.ricard@axible-tech.com>
2016-10-17 10:33:35 +0200

    Git ignore setup for build files

--- Sources/app/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var ci_module_1 = require('./ci.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(ci_module_1.AppModule);
-//# sourceMappingURL=main.js.map
