599a77b8ca14a27f050e9c6d21ef974004f96871
Jissay <jc.ricard@me.com>
2017-04-04 22:06:45 +0200

    Updated the Server start.sh script to include and create a local mongo database instead of having it in a root directory.

--- Server/start.sh
+++ Server/start.sh
@@ -1,5 +1,20 @@
+# ---------------------------------------------------------
 # Script to start a node server and a mongo daemon in a new 
-# terminal window, for macOS only. 
-osascript -e 'tell application "Terminal" to do script "mongod"'
+# terminal window, for macOS only.
+# ---------------------------------------------------------
+
+# We need the current path as a var to a database located in 
+# our local 'Server' directory
+path=`pwd`
+
+# Create the database directory if needed
+if [ ! -d "$path/database/db" ]
+    mkdir "$path/database/db"
+fi
+
+# Tells a new terminal window to run a mongo daemon
+osascript -e 'tell application "Terminal" to do script "mongod --dbpath '$path'/database/db"'
+
+# Create a new database from raw data and runs the server
 node ./database/createDB.js
 node ./bin/www
